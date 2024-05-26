60390e5d650b328f3e2dc2ab0d97ef88d5743f7c
darshan panchal <panchaldarshan121@gmail.com>
2024-05-26 15:34:30 +0530

    variables in go

---
+++ 02variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Darshan"
+	fmt.Println(name)
+
+	var age int = 22
+	fmt.Println("Age is: ", age)
+
+	location := "Pune"
+	fmt.Println("location is: ", location)
+
+	location = "India"
+	fmt.Println(location)
+
+	const isMarried = false
+	fmt.Println("Is married? ", isMarried)
+
+}
