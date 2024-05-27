be247d1836abc616256bca2c0e7a88041d4b399f
darshan panchal <panchaldarshan121@gmail.com>
2024-05-27 22:29:27 +0530

    structs in go

---
+++ 10structs/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Show struct {
+	Name        string
+	FemaleLead  string
+	MaleLead    string
+	IsCompleted bool
+}
+
+func main() {
+	DITF := Show{"Darling in the franxx", "zero two", "hiro", true}
+	fmt.Printf("show details are: %+v\n", DITF)
+
+	fmt.Println(DITF)
+	fmt.Println(DITF.FemaleLead)
+	fmt.Println(DITF.MaleLead)
+}
